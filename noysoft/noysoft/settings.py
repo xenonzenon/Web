91ab42c0c10dd69bc1c6b87f16cc9abbe334763e
Xenon Zenon <j5yson5b5d@gmail.com>
2023-03-04 17:35:50 +0800

    update 0.0.9

--- noysoft/noysoft/settings.py
+++ noysoft/noysoft/settings.py
@@ -3,7 +3,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -46,7 +45,7 @@ ROOT_URLCONF = 'noysoft.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [  ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
