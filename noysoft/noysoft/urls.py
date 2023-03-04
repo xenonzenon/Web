95668ec254872bda942276c606ff9401b53ac892
Xenon Zenon <j5yson5b5d@gmail.com>
2023-03-04 15:16:38 +0800

    0.0.8

--- noysoft/noysoft/urls.py
+++ noysoft/noysoft/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
+
 urlpatterns = [
+    path('favicon.png', RedirectView.as_view(url=staticfiles_storage.url('favicons/noysoft-16x16.png'))),
     path('admin/', admin.site.urls),
     path('', include('landing.urls')),
     path('noysoftapp/', include('noysoftapp.urls')),
